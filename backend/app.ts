99578e1cc1271b7c6717637981b47b0c6291182f
MoKhedim <mohammedkhedim123@gmail.com>
2023-01-29 10:39:22 -0500

    PostMedia complété

--- backend/app.ts
+++ backend/app.ts
@@ -1,16 +1,28 @@
 import express, { Request, response, Response } from 'express';
-const { Configuration, OpenAIApi } = require("openai");
+import MulterRequest from './models/MulterRequest';
+const { MongoClient, ServerApiVersion, GridFSBucket, Db } = require('mongodb');
 
-
+//require setup
 var bodyParser = require('body-parser');
-var app = express();
-var port = 3000;
+var fs = require('fs');
 var bcrypt = require('bcrypt');
+var axios = require('axios');
+var multer = require('multer');
+const upload = multer({ dest: "uploads/" });
+
+//express setup
+var app = express();
 const saltRounds = 10;
+var port = 3000;
+
+//app.use
 app.use(bodyParser.json());
 
-var axios = require('axios');
+//MONGO DB SETUP
+const uri = "mongodb+srv://momo:Oran2023@projetbecrazy.h0ghj.mongodb.net/?retryWrites=true&w=majority";
 
+//openai setup
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: "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"
   });
@@ -29,7 +41,7 @@ const configuration = new Configuration({
         return response.data.choices[0].text;
     }
 
-//route with ts type
+//routes with ts type
 app.post('/signup', (req:Request, res:Response) => {
     const today = new Date();
     const body = JSON.stringify(req.body);
@@ -68,40 +80,6 @@ app.post('/signup', (req:Request, res:Response) => {
 }
 );
 
-app.get('/login', (req:Request, res:Response) => {
-    const body = JSON.stringify(req.body);
-    console.log(body);
-    var data = JSON.stringify({
-        "collection": "users",
-        "database": "BeCrazy",
-        "dataSource": "ProjetBeCrazy",
-        "filter": {
-            "email": req.body.email,
-            "password": req.body.password
-        }
-    });
-    var config = {
-        method: 'post',
-        url: 'https://data.mongodb-api.com/app/data-diwam/endpoint/data/v1/action/find',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Request-Headers': '*',
-            'api-key': 'dneyLnyUcz1yBENLpVoHBMDo2rLn0S30yoOv3fVby1r5kFWK889gRSY03dybnrtV',
-            'Accept': 'application/ejson'
-        },
-        data: data
-    };
-
-    axios(config)
-    .then(function (response:any) {
-        console.log(JSON.stringify(response.data));
-        res.send(response.data);
-    })
-    .catch(function (error:any) {
-        console.log(error);
-    });
-    }
-);
 app.get("/aiChallenge", (req:Request, res:Response) =>{
     getCompletion().then((response:any) => {
         console.log(response);
@@ -113,44 +91,50 @@ app.get("/aiChallenge", (req:Request, res:Response) =>{
     );
 });
 
-app.post('/postMedia', (req:Request, res:Response) => {
-    const today = new Date();
-    const body = JSON.stringify(req.body);
-    console.log(body);
-    var data = JSON.stringify({
-        "collection": "allMedia",
-        "database": "BeCrazy",
-        "dataSource": "ProjetBeCrazy",
-        "document": {
-            "username": req.body.username,
-            "source": req.body.source,
-            "description": req.body.description,
-            "nbLike": 0,
-            "nbComment": 0,
-            "created": today
-        }
-    });
-    var config = {
-        method: 'post',
-        url: 'https://data.mongodb-api.com/app/data-diwam/endpoint/data/v1/action/insertOne',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Request-Headers': '*',
-            'api-key': 'dneyLnyUcz1yBENLpVoHBMDo2rLn0S30yoOv3fVby1r5kFWK889gRSY03dybnrtV',
-            'Accept': 'application/ejson'
-        },
-        data: data
-    };
-    axios(config)
-    .then(function (response:any) {
-        console.log(JSON.stringify(response.data));
-        res.send(response.data);
-    })
-    .catch(function (error:any) {
+let dbClient:typeof MongoClient;
+app.post('/postMedia', upload.single("video"), (req:Request, res:Response) => {
+  MongoClient.connect(uri, (err:Error, client:typeof MongoClient) => {
+    if(err) {
+      console.log(err);
+      res.status(500).json({ message: 'Error connecting to MongoDB' });
+      return;
+    }
+    dbClient = client;
+    const collection = client.db("BeCrazy").collection("users");
+    const bucket = new GridFSBucket(client.db("BeCrazy"), { bucketName: 'videos' });
+    const videoStream = fs.createReadStream((req as unknown as MulterRequest).file.path);
+    const uploadStream = bucket.openUploadStream((req as unknown as MulterRequest).file.originalname);
+
+    videoStream.pipe(uploadStream)
+    .on('error', (error:any) => {
         console.log(error);
+        res.status(500).json({ message: 'Error uploading video' });
+    })
+    .on('finish', (file:any) => {
+        fs.unlinkSync((req as unknown as MulterRequest).file.path);
+        res.status(200).json({ message: 'Video uploaded successfully' });
+        dbClient.close(); // close the connection once the operation is finished
+        res.send(file.id);
+    });        
+  });
+});
+
+app.get('/getMedia/:id', (req:Request, res:Response) => {
+    MongoClient.connect(uri, (err:Error, client:typeof MongoClient) => {
+        if(err) {
+            console.log(err);
+            res.status(500).json({ message: 'Error connecting to MongoDB' });
+            return;
+        }
+        dbClient = client;
+        const bucket = new GridFSBucket(client.db("BeCrazy"), { bucketName: 'videos' });
+        const downloadStream = bucket.openDownloadStream(req.params.id);
+        downloadStream.pipe(res); 
+        dbClient.close(); // close the connection once the operation is finished
     });
-}
-);
+});
+
+
 //listen 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
