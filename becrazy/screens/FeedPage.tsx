f53fe1647d54bf6f8d2e9d157c29f20034a87d8e
Henri Tran-Nguyen <henri.tran.nguyen@gmail.com>
2023-01-30 16:40:42 -0500

    setup userpost temporaire on feed

--- becrazy/screens/FeedPage.tsx
+++ becrazy/screens/FeedPage.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import { Image, Button } from 'react-native';
+import { Image, Button, ScrollView } from 'react-native';
 import Video from 'react-native-video';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
@@ -11,14 +11,28 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'FeedPag
     const colorScheme = useColorScheme();
 
     return (
-        <View style={styles.container}>
-            {
-                // error with react-native-video
-                //<Video source={require('../assets/videos/test.mp4')} onError={(error) => console.log(error)}/>
-            }
-            <View style={[styles.box, { backgroundColor: Colors[colorScheme].text }]} />
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                {
+                    // error with react-native-video
+                    //<Video source={require('../assets/videos/test.mp4')} onError={(error) => console.log(error)}/>
+                }
+                <View style={{ flexDirection: 'row' }}>
+                    <Image source={
+                        // user icon placeholder
+                        require('../assets/images/icon.png')} style={{
+                            width: 50,
+                            height: 50,
+                            borderRadius: 50,
+                            marginTop: 25
+                        }} />
+                    <Text style={[styles.name, { color: Colors[colorScheme].text, marginTop: 41, marginHorizontal: 10 }]}>User1</Text>
+                    <Text style={[styles.username, { color: Colors[colorScheme].text, marginTop: 42, marginEnd: 160, }]}>@username</Text>
+                </View>
+                <View style={[styles.box, { backgroundColor: Colors[colorScheme].text }]} />
+                <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            </View>
+        </ScrollView>
     );
 }
 
@@ -46,6 +60,15 @@ const styles = StyleSheet.create({
         padding: 10,
         width: 350,
         height: 550,
-        marginTop: 20,
+        marginTop: 10,
     },
+    name: {
+        fontSize: 14,
+        fontFamily: 'Century Gothic',
+        fontWeight: 'bold',
+    },
+    username: {
+        fontSize: 12,
+        fontFamily: 'Century Gothic',
+    }
 });
