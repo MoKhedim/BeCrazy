d216e7e98287d3df3f8f451dd533a966dda6a217
MoKhedim <mohammedkhedim123@gmail.com>
2023-02-10 16:50:09 -0500

    Ajout photo de profil et bio. Ajout route modification profil

--- backend/models/Users.ts
+++ backend/models/Users.ts
@@ -2,10 +2,11 @@ interface Users {
     username: string;
     email: string;
     password: string;
+    bio: string;
+    profilePicture: string;
     created: Date;
     token: string;
     nbFollows: number;
     nbFollowers: number;
 }
-
 export default Users;
