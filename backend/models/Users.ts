b1f95e0d3125e1c05da8a2a95e41ea977c30c7f9
Bast975 <bastiencambray975@gmail.com>
2023-02-20 15:24:28 -0500

    Revert "Ajout photo de profil et bio. Ajout route modification profil"
    This reverts commit d216e7e98287d3df3f8f451dd533a966dda6a217.

--- backend/models/Users.ts
+++ backend/models/Users.ts
@@ -2,11 +2,10 @@ interface Users {
     username: string;
     email: string;
     password: string;
-    bio: string;
-    profilePicture: string;
     created: Date;
     token: string;
     nbFollows: number;
     nbFollowers: number;
 }
+
 export default Users;
